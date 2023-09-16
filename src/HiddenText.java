ad9625db92aa0b5c2bcc20be0b6c33ac9c034843
jackalot <68657425+jackalot@users.noreply.github.com>
2023-09-16 18:26:38 -0400

    Added plan for HideOurText
    added a plan for HideOurText as well as a new format to do for the way the hidden text will display.

--- src/HiddenText.java
+++ src/HiddenText.java
@@ -2,6 +2,9 @@
 public class HiddenText {
 	 String text = "Sample text";
 	 public String HideOurText() {
+		 //Find a new word to hide
+		 //Hide our text with the format chosen
+		 //_*_*_*_*_-_*_*_*_*
 		 return text;
 	 }
 }
