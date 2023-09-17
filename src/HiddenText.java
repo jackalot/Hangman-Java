dd703cbd814612dffdd7aa9670ed9f9c3c668837
jackalot <68657425+jackalot@users.noreply.github.com>
2023-09-17 10:47:07 -0400

    Moved Code from FindNewWord to FindTheFile
    moved code from FindNewWord() to FindTheFile() as it fit that method better, as well as simplified FindNewWord to one function.

--- src/HiddenText.java
+++ src/HiddenText.java
@@ -4,10 +4,6 @@ import java.util.Scanner;
 public class HiddenText {
 	 String text = "Sample text";
 	 private void FindTheFile() {
-		 
-	 }
-	 // Finds a new word for us from the "Words to Hide" file 
-	 private void FindNewWord() {
 		 try
 		 {
 			 File wordObj = new File("Words to Hide"); //Grab the file
@@ -21,11 +17,15 @@ public class HiddenText {
 		 catch (FileNotFoundException e){
 			System.out.print(e);
 		 }
+	 }
+	 // Finds a new word for us from the "Words to Hide" file 
+	 private void FindNewWord() {
 		 
 	 
 	 }
 	 public String HideOurText() {
 		 //Find a new word to hide
+		 
 		 //Hide our text with the format chosen
 		 //_ _ _ _ _ _ _ _	 _ _ _   _ _ _ _
 		 return text;
