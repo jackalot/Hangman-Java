fae0b6a17749280c193d40a127f6dfaa4fb94d33
jackalot <68657425+jackalot@users.noreply.github.com>
2023-09-17 10:39:37 -0400

    FindNewWord scans lines now
    FindNewWord now scans each line within a while loop and both stores it, and prints it.

--- src/HiddenText.java
+++ src/HiddenText.java
@@ -9,6 +9,11 @@ public class HiddenText {
 		 {
 			 File wordObj = new File("Words to Hide"); //Grab the file
 			 Scanner myReader =  new Scanner(wordObj); //Start scanning it
+			 while(myReader.hasNextLine()) // while it has more lines to read
+			 {
+				 String data = myReader.nextLine(); // the next line gets stored to data
+				 System.out.print(data); // print it
+			 }
 		 }
 		 catch (FileNotFoundException e){
 			System.out.print(e);
