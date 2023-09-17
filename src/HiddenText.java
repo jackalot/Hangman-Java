ce506e6bf19339727f238af1ec6299989ee5f8e9
jackalot <68657425+jackalot@users.noreply.github.com>
2023-09-17 10:18:12 -0400

    created FindNewWord()
    created a new method within Hidden Text class, findNew word, added description comment for it too.

--- src/HiddenText.java
+++ src/HiddenText.java
@@ -1,6 +1,10 @@
 
 public class HiddenText {
 	 String text = "Sample text";
+	 // Finds a new word for us from the "Words to Hide" file 
+	 private void FindNewWord() {
+	 
+	 }
 	 public String HideOurText() {
 		 //Find a new word to hide
 		 //Hide our text with the format chosen
