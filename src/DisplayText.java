9398d7cb8e877070bb73abb406dc446f503b60a6
jackalot <68657425+jackalot@users.noreply.github.com>
2023-09-21 08:46:04 -0400

    Added plan to CreateWindow()
    added a plan to CreateWindow()

--- src/DisplayText.java
+++ src/DisplayText.java
@@ -3,7 +3,11 @@ public class DisplayText {
 	char[] Guesses;
 	public void CreateWindow(HiddenText OurWords)
 	{
-		/*
-		 * */
+		
+		 // prompt the user for a character they think is in the word
+		// check if the character is in the word
+		 // If it was right, add the players guess
+		 // display the new prompt
+		 
 	}
 }
