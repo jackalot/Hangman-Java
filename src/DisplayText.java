4177dc23a3acee2d32911402ba3475739f255ec2
jackalot <68657425+jackalot@users.noreply.github.com>
2023-09-21 09:03:22 -0400

    Created Variable OurPrompt
    added new variable OurPrompt to store the prompt this class will display.

--- src/DisplayText.java
+++ src/DisplayText.java
@@ -1,12 +1,12 @@
 
 public class DisplayText {
 	char[] Guesses;
+	String OurPrompt;
 	public char GuessCharacter(HiddenText OurWords) {
 	return ' ';
 	}
 	public void CreateWindow(HiddenText OurWords)
 	{
-		
 		 // prompt the user for a character they think is in the word
 		// check if the character is in the word
 		 // If it was right, add the players guess
