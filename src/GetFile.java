1b12c873f3164f5e86f04cc9046e0f53e218c2c9
jackalot <68657425+jackalot@users.noreply.github.com>
2023-09-28 12:47:01 -0400

    HiddenTexts's FindTheFile is now in GetFile
    moved the FindTheFile() method to GetFile, so that its in it's own java file and easier to test.

--- src/GetFile.java
+++ src/GetFile.java
@@ -1,4 +1,24 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class GetFile {
-
+	public List<String> FindTheFile() {
+		List<String> StoredWords = new ArrayList<String>();
+		try {
+			File wordObj = new File("src/WordsToHide"); // Grab the file
+			Scanner myReader = new Scanner(wordObj); // Start scanning it
+			while (myReader.hasNextLine()) // while it has more lines to read
+			{
+				String data = myReader.nextLine(); // the next line gets stored to data
+				StoredWords.add(data);
+			}
+			myReader.close();
+		} catch (FileNotFoundException e) {
+			System.out.print(e);
+		}
+		return StoredWords;
+	}
 }
