b92ecf7374c28ee33fe18736b201c940a647e827
jackalot <68657425+jackalot@users.noreply.github.com>
2023-09-28 12:55:20 -0400

    changed FindTheFiles return type
    FindTheFile now has the return type of File

--- src/GetFile.java
+++ src/GetFile.java
@@ -6,7 +6,8 @@ import java.util.Scanner;
 
 public class GetFile {
 	File OurFile;
-	public List<String> FindTheFile() {
+	
+	public File FindTheFile() {
 		List<String> StoredWords = new ArrayList<String>();
 		 OurFile = new File("src/WordsToHide"); // Grab the file
 		return OurFile;
