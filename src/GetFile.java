445dfec4ddda6fd923169ba3aec922f4963defda
jackalot <68657425+jackalot@users.noreply.github.com>
2023-09-28 12:53:48 -0400

    Removed try catch of FindTheFile
    find the file now only has one purpose, which is to only find the file. scanning the file will be made for another method.

--- src/GetFile.java
+++ src/GetFile.java
@@ -5,20 +5,10 @@ import java.util.List;
 import java.util.Scanner;
 
 public class GetFile {
+	File OurFile;
 	public List<String> FindTheFile() {
 		List<String> StoredWords = new ArrayList<String>();
-		try {
-			File wordObj = new File("src/WordsToHide"); // Grab the file
-			Scanner myReader = new Scanner(wordObj); // Start scanning it
-			while (myReader.hasNextLine()) // while it has more lines to read
-			{
-				String data = myReader.nextLine(); // the next line gets stored to data
-				StoredWords.add(data);
-			}
-			myReader.close();
-		} catch (FileNotFoundException e) {
-			System.out.print(e);
-		}
-		return StoredWords;
+		 OurFile = new File("src/WordsToHide"); // Grab the file
+		return OurFile;
 	}
 }
