1ff8b1bdcf34cbf46447b0a52357d8c5487f79a7
jackalot <68657425+jackalot@users.noreply.github.com>
2023-09-28 13:30:08 -0400

    Updated ScanTheFile()
    ScanTheFile is closer to what it originally did, now it will scan each line in our file.

--- src/GetFile.java
+++ src/GetFile.java
@@ -8,13 +8,20 @@ public class GetFile {
 	File OurFile;
 	public List<String> ScanTheFile() {
 		List<String> StoredWords = new ArrayList<String>();
-		Scanner myReader = new Scanner(wordObj); // Start scanning it
-		while (myReader.hasNextLine()) // while it has more lines to read
-		{
-			String data = myReader.nextLine(); // the next line gets stored to data
-			StoredWords.add(data);
-		}
-		myReader.close();
+		OurFile = new File("src/WordsToHide");
+		Scanner myReader;
+		try {
+			myReader = new Scanner(OurFile);
+			while (myReader.hasNextLine()) // while it has more lines to read
+			{
+				String data = myReader.nextLine(); // the next line gets stored to data
+				StoredWords.add(data);
+			}
+			myReader.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} // Start scanning it
 		return StoredWords;
 	}
 	public File FindTheFile() {
