b9a8d793a8169bfd0173c82d186e2384a0fe0ba0
jackalot <68657425+jackalot@users.noreply.github.com>
2023-09-28 13:20:41 -0400

    ScanTheFile now scanes our file
    Scan the file has our previous  scanning function

--- src/GetFile.java
+++ src/GetFile.java
@@ -7,7 +7,16 @@ import java.util.Scanner;
 public class GetFile {
 	File OurFile;
 	public List<String> ScanTheFile() {
-			}
+		List<String> StoredWords = new ArrayList<String>();
+		Scanner myReader = new Scanner(wordObj); // Start scanning it
+		while (myReader.hasNextLine()) // while it has more lines to read
+		{
+			String data = myReader.nextLine(); // the next line gets stored to data
+			StoredWords.add(data);
+		}
+		myReader.close();
+		return StoredWords;
+	}
 	public File FindTheFile() {
 		 OurFile = new File("src/WordsToHide"); // Grab the file
 		return OurFile;
